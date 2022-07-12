09aa45e3b4f9071c182d1d816ead07c4eb0e2780
reeshaa <shenoyreesha@gmail.com>
2022-07-12 15:45:48 +0530

    firebase configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:plant_health_tracker/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
